6702a65503777a62f592a28ae14be18051f4690f
heshu2026 <heshu@chipltech.com>
2026-06-02 13:00:43 +0800

    first push

---
+++ test/infer.py
@@ -0,0 +1,14 @@
+import torch
+import numpy
+from torch.nn import Conv2d
+
+def generate_test_data():
+    pass
+
+def align(groundtruth, result):
+
+
+if __name__ == '__main__':
+    input = torch.randn(3, 224, 224)
+    conv1 = Conv2d(3, 16, 4)
+    output = conv1(input)
